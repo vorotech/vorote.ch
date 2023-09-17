3ecc7890aae30474b5b7097b2216c909127e9477
vorotech <2937451+vorotech@users.noreply.github.com>
2023-09-17 10:13:50 +0300

    fix(commitlint): update commitlint config to ignore dependabot commits

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    ignores: [(message) => /^Bumps \[(.+)\]\((.+)\)(.*).$/m.test(message)],
+    rules: {
+      'scope-case': [2, 'always', ['pascal-case', 'lower-case', 'camel-case']],
+    },
+  }
