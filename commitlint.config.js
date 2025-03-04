60e46eebee049d720690106626b7b808e45826ea
Dmytro <2937451+vorotech@users.noreply.github.com>
2025-03-04 15:16:38 +0200

    chore(ci): update commitlint config to handle dependabot commits (#156)
    Ignore message length for dependabot PRs since they are auto-generated.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  ignores: [(message) => /^Bumps \[(.+)\]\((.+)\)(.*).$/m.test(message)],
+  ignores: [
+    (message) => /^Bumps \[(.+)\]\((.+)\)(.*).$/m.test(message),
+    (message) => /^chore\(deps\): bump/i.test(message),
+  ],
   rules: {
     "scope-case": [2, "always", ["pascal-case", "lower-case", "camel-case"]],
   },
